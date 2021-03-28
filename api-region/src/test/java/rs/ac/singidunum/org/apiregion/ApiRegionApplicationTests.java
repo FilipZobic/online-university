4a54a3d0d9410efe6405ea9dacfa50ec3e684315
FilipZobic <zobicfilip919@gmail.com>
2021-03-28 16:37:43 +0200

    Added api-region.

---
+++ api-region/src/test/java/rs/ac/singidunum/org/apiregion/ApiRegionApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.ac.singidunum.org.apiregion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRegionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
